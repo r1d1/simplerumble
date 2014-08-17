6e5108083a810b31b9751629c7a22fe4a065d2be
r1d1 <r1d1@users.noreply.github.com>
2014-08-17 23:21:47 +0200

    Adding Map also

---
+++ Map.h
@@ -0,0 +1,57 @@
+/*
+ *  Map.h
+ *  SimpleRumble
+ *
+ *  Created by R1M@ster on 15/08/14.
+ *  Copyright 2014 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#ifndef MAP_H
+#define MAP_H
+
+// Map Size in tiles :
+#define TILE_WIDTH 32
+#define TILE_HEIGHT 32
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include "Tile.h"
+
+class Map
+{
+	private:
+		unsigned int tileNumber;
+		std::vector<Tile> tileSet; // ? eq. chipset
+		//sf::Sprite * bgSprite; // eq. surf_chipset
+		//sf::Image * bgSprite; // eq. surf_chipset
+		//sf::Image bgSprite; // eq. surf_chipset
+		sf::Texture bgSprite; // eq. surf_chipset
+
+		sf::Vector2<unsigned int> mapSize;
+		unsigned int mapWidth;
+		unsigned int mapHeight;
+		unsigned int * data; // Probablement à améliorer
+		//std::vector<unsigned int> data; // Probablement à améliorer
+
+		sf::Vector2<unsigned int> offsetMap;
+		unsigned int offsetX;
+		unsigned int offsetY;
+
+		//sf::Music ???
+
+
+	public:
+		Map();
+		~Map();
+		//getTile(int x, int y, map);
+		//Tile getTile(int x, int y);
+		unsigned int getTile(int x, int y);
+		void randomizeMap(int width, int height, const std::string tilesetname);
+};
+
+#endif
